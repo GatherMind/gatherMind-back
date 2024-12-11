4b7a958ad21ead6ab5daee31a5367f2fca0b1077
rachael92724 <73717701+rachael92724@users.noreply.github.com>
2024-12-11 18:29:48 +0900

    add files/upload controller

--- src/main/java/woongjin/gatherMind/controller/FileController.java
+++ src/main/java/woongjin/gatherMind/controller/FileController.java
@@ -1,9 +1,20 @@
 package woongjin.gatherMind.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import woongjin.gatherMind.DTO.FileUploadResponseDTO;
+import woongjin.gatherMind.config.JwtTokenProvider;
+import woongjin.gatherMind.service.FileService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -12,6 +23,29 @@ import org.springframework.web.bind.annotation.*;
 public class FileController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+    private final FileService fileService;
+    private final JwtTokenProvider jwtTokenProvider;
 
+    @Operation(
+            summary = "파일 업로드",
+            description = "에디터를 통해 첨부된 이미지 파일을 업로드합니다."
+    )
+    @PostMapping("/upload")
+    public ResponseEntity<List<String>> uploadFiles(HttpServletRequest request,
+                                                    @RequestParam("files[]") List<MultipartFile> files) {
+        String memberId = jwtTokenProvider.extractMemberIdFromRequest(request);
+        List<String> fileUrls = new ArrayList<>();
+        Boolean isContentEmbedded = true;
+        try {
+            for (MultipartFile file : files) {
+                FileUploadResponseDTO responseDTO = fileService.handleFileUpload(file, memberId, isContentEmbedded);
+                fileUrls.add(responseDTO.getFileUrl());
+            }
+            return ResponseEntity.ok(fileUrls);
+        } catch (Exception e) {
+            logger.error("파일 업로드 실패: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 }
